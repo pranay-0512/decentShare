64fcee2035e270c60ad1590d3acbb010f3685e44
pranay_0512 <linkpranayv90@gmail.com>
2024-11-17 23:34:09 +0000

    done

--- connection/tcp.go
+++ connection/tcp.go
@@ -2,28 +2,56 @@ package connection
 
 import (
 	"fmt"
-	"log"
-	"net"
+	"sync"
+
+	"github.com/pion/stun"
 )
 
 func StartTCPconnection() {
-	listener, err := net.Listen("tcp", ":1234")
-	if err != nil {
-		log.Println("error listening to tcp connection", err)
-	}
-	defer listener.Close()
-	fmt.Println("Listening on port: 1234")
-	for {
-		fmt.Println("Waiting for a connnection to accept")
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Println("error listening to tcp connection", err)
-		}
-		defer conn.Close()
-		fmt.Println("Connecion established!")
-	}
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go getSTUN(&wg)
+	wg.Wait()
+
+	// listener, err := net.Listen("tcp", "115.245.205.158")
+	// if err != nil {
+	// 	log.Println("error listening to tcp connection", err)
+	// }
+	// defer listener.Close()
+	// fmt.Println("Listening on port: 6769")
+	// for {
+	// 	fmt.Println("Waiting for a connnection to accept")
+	// 	conn, err := listener.Accept()
+	// 	if err != nil {
+	// 		log.Println("error listening to tcp connection", err)
+	// 	}
+	// 	defer conn.Close()
+	// 	fmt.Println("Connecion established!")
+	// }
 }
 
-func readAndWrite(conn net.Conn) {
-	// 
+func getSTUN(wg *sync.WaitGroup) {
+	defer wg.Done()
+	u, err := stun.ParseURI("stun:stun.l.google.com:19302")
+	if err != nil {
+		panic(err)
+	}
+
+	c, err := stun.DialURI(u, &stun.DialConfig{})
+	if err != nil {
+		panic(err)
+	}
+	message := stun.MustBuild(stun.TransactionID, stun.BindingRequest)
+	if err := c.Do(message, func(res stun.Event) {
+		if res.Error != nil {
+			panic(res.Error)
+		}
+		var xorAddr stun.XORMappedAddress
+		if err := xorAddr.GetFrom(res.Message); err != nil {
+			panic(err)
+		}
+		fmt.Println("your IP is", xorAddr.IP)
+	}); err != nil {
+		panic(err)
+	}
 }
--- main.go
+++ main.go
@@ -3,6 +3,22 @@ package main
 import "p2p/connection"
 
 func main() {
+	// when main function is called (the entry point),
+	// it means the user wants to become a peer
+	// meaning it will either be a leecher or a seeder
+	// user can select which file it needs to seed (seeder state)
+	// user will get a list of files that he can leech (leecher state)
+
+	// once he selects the file to either seed or leech, he will talk to DHT to find out the kv store of that file.
+	// meaning he will have the public ips of all the peers already in the peer pool.
+	// the choking algorithm will come into play and he will be randomly unchoked.
+	// before unchoking the user, a peer will setup a TCP connection with the user.
+	// Then he will send a random piece of file to the user. 
+	// The connection will not close untill all the pieces are trasnfered. //TODO - maybe the connection will only end if a peer abruptly leaves.
+	// Connection will not be closed further.
+	// A peer can handle more than 20000 TCP connections (both idle and active) at once.
+	// The open connection will help in sending have messages and bitfields
+	// 
 	connection.StartTCPconnection()
 }
 
