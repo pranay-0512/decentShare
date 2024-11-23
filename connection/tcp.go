0f0745a15618d33503a19d92a1b5d97b6ff0b0bc
Pranay Varma <linkpranayv90@gmail.com>
2024-11-23 17:33:04 +0530

    file sharing V1 done

--- connection/tcp.go
+++ connection/tcp.go
@@ -1,106 +1,106 @@
 package connection
 
-import (
-	"io"
-	"log"
-	"net"
-	"os"
-	"p2p/file"
-	"time"
-)
+// import (
+// 	"io"
+// 	"log"
+// 	"net"
+// 	"os"
+// 	"p2p/file"
+// 	"time"
+// )
 
-var filename = "C:/Users/linkp/OneDrive/Desktop/decentShare/connection/main.txt"
+// var filename = "C:/Users/linkp/OneDrive/Desktop/decentShare/connection/main.txt"
 
-// StartTCPConnection sets up a TCP server to send file chunks
-func StartTCPConnection() {
-	listener, err := net.Listen("tcp", ":5555")
-	if err != nil {
-		log.Fatalf("Error setting up TCP listener: %v", err)
-	}
-	defer listener.Close()
+// // StartTCPConnection sets up a TCP server to send file chunks
+// func StartTCPConnection() {
+// 	listener, err := net.Listen("tcp", ":5555")
+// 	if err != nil {
+// 		log.Fatalf("Error setting up TCP listener: %v", err)
+// 	}
+// 	defer listener.Close()
 
-	log.Println("Server listening on :5555")
+// 	log.Println("Server listening on :5555")
 
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Printf("Error accepting connection: %v", err)
-			continue
-		}
-		go handleConnection(conn)
-	}
-}
+// 	for {
+// 		conn, err := listener.Accept()
+// 		if err != nil {
+// 			log.Printf("Error accepting connection: %v", err)
+// 			continue
+// 		}
+// 		go handleConnection(conn)
+// 	}
+// }
 
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-	log.Printf("Connection established with %s", conn.RemoteAddr())
+// func handleConnection(conn net.Conn) {
+// 	defer conn.Close()
+// 	log.Printf("Connection established with %s", conn.RemoteAddr())
 
-	chunks, err := file.Chunkify(filename)
-	if err != nil {
-		log.Printf("Error chunkifying file: %v", err)
-		return
-	}
+// 	chunks, err := file.Chunkify(filename)
+// 	if err != nil {
+// 		log.Printf("Error chunkifying file: %v", err)
+// 		return
+// 	}
 
-	start := time.Now()
-	for i, chunk := range chunks {
-		n, err := conn.Write(chunk)
-		if err != nil {
-			log.Printf("Error sending chunk %d: %v", i, err)
-			return
-		}
-		log.Printf("Sent chunk %d of size %d bytes", i, n)
-	}
-	duration := time.Since(start)
-	log.Printf("File transfer completed in %v", duration)
-}
+// 	start := time.Now()
+// 	for i, chunk := range chunks {
+// 		n, err := conn.Write(chunk)
+// 		if err != nil {
+// 			log.Printf("Error sending chunk %d: %v", i, err)
+// 			return
+// 		}
+// 		log.Printf("Sent chunk %d of size %d bytes", i, n)
+// 	}
+// 	duration := time.Since(start)
+// 	log.Printf("File transfer completed in %v", duration)
+// }
 
-// DialTCP sets up a TCP client to receive file chunks
-func DialTCP() {
-	conn, err := net.Dial("tcp", "192.168.1.166:5555")
-	if err != nil {
-		log.Fatalf("Error connecting to server: %v", err)
-	}
-	defer conn.Close()
+// // DialTCP sets up a TCP client to receive file chunks
+// func DialTCP() {
+// 	conn, err := net.Dial("tcp", "192.168.1.166:5555")
+// 	if err != nil {
+// 		log.Fatalf("Error connecting to server: %v", err)
+// 	}
+// 	defer conn.Close()
 
-	log.Println("Connected to server")
+// 	log.Println("Connected to server")
 
-	var chunks [][]byte
-	buf := make([]byte, file.ChunkSize)
-	start := time.Now()
+// 	var chunks [][]byte
+// 	buf := make([]byte, file.ChunkSize)
+// 	start := time.Now()
 
-	for {
-		bytesRead, err := conn.Read(buf)
-		if err != nil {
-			if err == io.EOF {
-				log.Println("All chunks received")
-				break
-			}
-			log.Printf("Error reading from connection: %v", err)
-			return
-		}
+// 	for {
+// 		bytesRead, err := conn.Read(buf)
+// 		if err != nil {
+// 			if err == io.EOF {
+// 				log.Println("All chunks received")
+// 				break
+// 			}
+// 			log.Printf("Error reading from connection: %v", err)
+// 			return
+// 		}
 
-		chunk := make([]byte, bytesRead)
-		copy(chunk, buf[:bytesRead])
-		chunks = append(chunks, chunk)
-		log.Printf("Received chunk of size %d bytes", bytesRead)
-	}
-	duration := time.Since(start)
-	log.Printf("File transfer completed in %v", duration)
+// 		chunk := make([]byte, bytesRead)
+// 		copy(chunk, buf[:bytesRead])
+// 		chunks = append(chunks, chunk)
+// 		log.Printf("Received chunk of size %d bytes", bytesRead)
+// 	}
+// 	duration := time.Since(start)
+// 	log.Printf("File transfer completed in %v", duration)
 
-	outputFile := "C:/Users/linkp/OneDrive/Desktop/decentShare/connection/received_file.txt"
+// 	outputFile := "C:/Users/linkp/OneDrive/Desktop/decentShare/connection/received_file.txt"
 
-	// Ensure the output file is created if not present
-	outputFileHandle, err := os.OpenFile(outputFile, os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		log.Fatalf("Error creating or opening output file: %v", err)
-	}
-	outputFileHandle.Close() // Close immediately, as we will write during merging
+// 	// Ensure the output file is created if not present
+// 	outputFileHandle, err := os.OpenFile(outputFile, os.O_RDWR|os.O_CREATE, 0666)
+// 	if err != nil {
+// 		log.Fatalf("Error creating or opening output file: %v", err)
+// 	}
+// 	outputFileHandle.Close() // Close immediately, as we will write during merging
 
-	err = file.Merge(outputFile, chunks)
-	if err != nil {
-		log.Printf("Error merging file: %v", err)
-		return
-	}
+// 	err = file.Merge(outputFile, chunks)
+// 	if err != nil {
+// 		log.Printf("Error merging file: %v", err)
+// 		return
+// 	}
 
-	log.Printf("File successfully reconstructed as: %s", outputFile)
-}
+// 	log.Printf("File successfully reconstructed as: %s", outputFile)
+// }
--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-V1: Make sure 2 peers can send and receive files - Use TCP
+V1: Make sure 2 peers can send and receive files - Use TCP --DONE
 V2: Reciprocating, Choking and Interested algorithm
 V3: Peer discovery & Kademlia DHT
 V4: NAT Traversals
@@ -18,23 +18,23 @@ func main() {
 	peer := network.NewPeer(networkConfig)
 
 	// Option 1: Listen for incoming connections
-	go func() {
-		if err := peer.Listen(); err != nil {
-			log.Fatalf("Listening error: %v", err)
-		}
-	}()
+	// go func() {
+	// 	if err := peer.Listen(); err != nil {
+	// 		log.Fatalf("Listening error: %v", err)
+	// 	}
+	// }()
 
 	// Option 2: Send a file to a specific peer
-	go func() {
-		err := peer.DialAndSendFile("192.168.1.166:5555", "/path/to/file.txt")
-		if err != nil {
-			log.Printf("File send error: %v", err)
-		}
-	}()
+	// go func() {
+	// 	err := peer.DialAndSendFile("192.168.1.166:5555", "C:/Users/linkp/OneDrive/Desktop/decentShare/main.txt")
+	// 	if err != nil {
+	// 		log.Printf("File send error: %v", err)
+	// 	}
+	// }()
 
 	// Option 3: Receive a file
 	go func() {
-		err := peer.ReceiveFile("/path/to/received_file.txt")
+		err := peer.ReceiveFile("C:/Users/linkp/OneDrive/Desktop/decentShare/main.txt")
 		if err != nil {
 			log.Printf("File receive error: %v", err)
 		}
